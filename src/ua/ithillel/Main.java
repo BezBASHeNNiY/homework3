fc0791def69ffc17edd3f51a68e086a118092433
Artem <volcharartem@gmail.com>
2022-10-30 23:17:20 +0200

    Done hw

--- src/ua/ithillel/Main.java
+++ src/ua/ithillel/Main.java
@@ -1,6 +1,12 @@
 package ua.ithillel;
 
 public class Main {
-    public static void main(String[] args) {;
+    public static void main(String[] args) {
+
+        Employee emp1 = new Employee("Test", "test@test.test", "test", 55, 123456879);
+
+        Car car1 = new Car();
+        car1.start();
+
     }
 }
